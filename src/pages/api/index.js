9ef861652d572d938fdadc6605e3e4fce167b4ca
Sebastian Alsina <alsinas@me.com>
2021-04-06 03:00:41 -0400

    Cors Policy Added

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,10 +1,27 @@
 var fs = require("fs");
 var mobileconfig = require("mobileconfig");
 
-export default (req, res) => {
+const allowCors = (fn) => async (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
 
+const handler = (req, res) => {
     mobileconfig.getEmailConfig(req.body, (err, data) => {
         if (err) {
             console.log(err);
@@ -26,3 +43,5 @@ export default (req, res) => {
         });
     });
 };
+
+export default allowCors(handler);
