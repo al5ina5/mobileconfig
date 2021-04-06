6aa98003a506ebf559c8d8a45b7284e2abd26da1
Sebastian Alsina <alsinas@me.com>
2021-04-06 02:58:20 -0400

    Changed CORS Policy

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,23 +1,28 @@
-
-var fs = require('fs')
-var mobileconfig = require('mobileconfig')
+var fs = require("fs");
+var mobileconfig = require("mobileconfig");
 
 export default (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     mobileconfig.getEmailConfig(req.body, (err, data) => {
         if (err) {
-            console.log(err)
-            return
+            console.log(err);
+            return;
         }
 
         if (!req.body) {
             return res.send({
-                message: 'Error! View https://mobileconfig.vercel.app to information on how to utilize this API.'
-            })
+                message:
+                    "Error! View https://mobileconfig.vercel.app to information on how to utilize this API.",
+            });
         }
 
         res.send({
-            download: `https://mobileconfig.vercel.app/api/download/${encodeURIComponent(JSON.stringify(req.body))}`,
-            mobileConfigFile: data
-        })
-    })
-}
+            download: `https://mobileconfig.vercel.app/api/download/${encodeURIComponent(
+                JSON.stringify(req.body)
+            )}`,
+            mobileConfigFile: data,
+        });
+    });
+};
